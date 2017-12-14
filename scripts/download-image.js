64f10401b9d41ff04813b7206e8d290a39dd779a
Herson Salinas <hersonhn@gmail.com>
2017-12-14 17:41:11 -0600

    Download image script

---
+++ scripts/download-image.js
@@ -0,0 +1,36 @@
+
+const Path = require('path');
+const wget = require('node-wget');
+const config = require('../config.json');
+
+// trailing slash is important for wget api
+const tmp = Path.resolve(__dirname, '../tmp') + '/';
+
+
+main();
+
+
+function main() {
+  // if running from the command line
+  if (require.main === module) {
+    downloadImage('https://goo.gl/KudZVD')
+      .then(console.log);
+  }
+}
+
+
+function downloadImage(image) {
+  return new Promise(function(resolve, reject) {
+    wget({ url: image, dest: tmp }, function (error, response, body) {
+      if (error) return reject(error);
+
+      let file = Path.resolve(response.filepath);
+      let url = image;
+      console.log(`Downloaded image ${url} to ${file}`);
+
+      resolve({ file: file, url: url });
+    })
+  });
+}
+
+module.exports = downloadImage;
